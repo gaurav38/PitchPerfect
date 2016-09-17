1924930f81185b805be8783bff4eded289e6a646
Gaurav Saraf <grv_srf@yahoo.co.in>
2016-09-17 12:33:37 -0400

    Added stop and record buttons and images

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -15,11 +15,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var recordingLabel: UILabel!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAction(_ sender: AnyObject) {
+        print("record was pressed.")
+        recordingLabel.text = "Recording in progress."
+    }
+    
+    @IBAction func stopRecording(_ sender: AnyObject) {
+        print("Stop recording button pressed")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("view will appear")
+    }
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
 }
 
