b8a7b061b4a710d5250dd76a16d5b798a1bda0c3
Gaurav Saraf <grv_srf@yahoo.co.in>
2016-09-18 18:23:03 -0400

    Added code behind for buttons

--- PlaySoundsViewController.swift
+++ PlaySoundsViewController.swift
@@ -10,12 +10,52 @@ import UIKit
 
 class PlaySoundsViewController: UIViewController {
 
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    enum ButtonType: Int { case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb }
+    
+    @IBAction func playSoundForButton(sender: UIButton) {
+        print("Play sound button presssed")
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .Slow:
+            playSound(rate: 0.5)
+        case .Fast:
+            playSound(rate: 1.5)
+        case .Chipmunk:
+            playSound(pitch: 1000)
+        case .Vader:
+            playSound(pitch: -1000)
+        case .Echo:
+            playSound(echo: true)
+        case .Reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(playState: .Playing)
+    }
+    
+    @IBAction func stopButtonPressed(sender: AnyObject) {
+        print("Stop audio button pressed")
+        stopAudio()
+    }
+    
     var recorderAudioURL: NSURL!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("PlaySoundsViewController loaded")
+        setupAudio()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(playState: .NotPlaying)
     }
 
     override func didReceiveMemoryWarning() {
